49326e436b7368172faaa44dbdd247243dbc5e50
suisoh <88411618+ssuish@users.noreply.github.com>
2024-05-19 17:42:01 +0800

    Enemy animations

---
+++ DinoPeers_Farm/Assets/EnemyAC.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAC : MonoBehaviour
+{
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FlipTowardsPlayer();
+    }
+    
+    void FlipTowardsPlayer()
+    {
+        if (player.position.x < transform.position.x)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else
+        {
+            _spriteRenderer.flipX = true;
+        }
+    }
+}
