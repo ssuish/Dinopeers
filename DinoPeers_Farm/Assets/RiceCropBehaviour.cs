27b2a7472d91430329f60d47d1ae1f83e6313539
HydGea <115512432+HydGea@users.noreply.github.com>
2024-05-19 16:54:29 +0800

    Add Crop Growth

--- DinoPeers_Farm/Assets/RiceCropBehaviour.cs
+++ DinoPeers_Farm/Assets/RiceCropBehaviour.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class RiceCropBehaviour : MonoBehaviour
 {
+
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] spriteArray;
+
     int RicemaxHP = 100;
     int RiceHP = 100;
 
@@ -13,16 +17,39 @@ public class RiceCropBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        gameTimer -= Time.deltaTime;
         if (RiceHP <= 0)
         {
             Debug.Log("Crop Died");
             DestroyCrop();
+        }
+        riceGrowth();
+     
+    }
+
+    void riceGrowth()
+    {
+        if (gameTimer <= 1200f && gameTimer > 820f && RiceHP > 0)
+        {
+            spriteRenderer.sprite = spriteArray[0];
+        }
+        else if (gameTimer <= 820f && gameTimer > 440f && RiceHP > 0)
+        {
+            spriteRenderer.sprite = spriteArray[1];
+        }
+        else if (gameTimer <= 440f && gameTimer > 60f && RiceHP > 0)
+        {
+            spriteRenderer.sprite = spriteArray[2];
+        }
+        else if (gameTimer <= 60f && RiceHP > 0)
+        {
+            spriteRenderer.sprite = spriteArray[3];
         }
     }
 
