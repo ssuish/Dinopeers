62cc621722f26d870fb9651a8286fa19e4deb52b
suisoh <88411618+ssuish@users.noreply.github.com>
2024-05-19 11:01:17 +0800

    Fixed player movement and boundary collisions.

--- DinoPeers_Farm/Assets/Scripts/PlayerMovement.cs
+++ DinoPeers_Farm/Assets/Scripts/PlayerMovement.cs
@@ -1,54 +1,104 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 10f;
+    public float moveSpeed = 1f;
     public bool isMoving;
-    public Vector2 input;
+    private Vector2 input;
     private Animator _animator;
     private SpriteRenderer _spriteRenderer;
-    private BoxCollider2D _boxCollider;
-
-    public LayerMask boundaryLayer; 
+    
+    public float collisionOffset = 0.05f;
+    public ContactFilter2D movementContactFilter;
+    private Rigidbody2D _rigidbody2D;
+    List<RaycastHit2D> _hitBuffer = new List<RaycastHit2D>(16);
+    private bool _canMove = true;
+    private PlayerInput _playerInput;
 
     // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponent<Animator>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
-        _boxCollider = GetComponent<BoxCollider2D>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _playerInput = GetComponent<PlayerInput>();
+      
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (!isMoving)
+        if (_canMove)
         {
-            input.x = Input.GetAxis("Horizontal");
-            input.y = Input.GetAxis("Vertical");
-
+            input = _playerInput.actions["Move"].ReadValue<Vector2>();
+            
             if (input.x != 0)
             {
                 _spriteRenderer.flipX = input.x > 0;
             }
 
-            if (input != Vector2.zero)
+            if (input != Vector2.zero)  
             {
-                var targetPos = transform.position;
-                targetPos.x += input.x;
-                targetPos.y += input.y;
+                bool success = TryMove(input);
 
-                if (CanMoveTo(targetPos))
+                if (!success)
                 {
-                    StartCoroutine(Move(targetPos));
+                    success = TryMove(new Vector2(input.x, 0));
+                    if (!success)
+                    {
+                        success = TryMove(new Vector2(0, input.y));
+                    }
                 }
+
+                _animator.SetBool("IsMoving", success);
+            }
+            else
+            {
+                _animator.SetBool("IsMoving", false);
             }
         }
     }
 
-    private bool CanMoveTo(Vector3 targetPos)
+    private bool TryMove(Vector2 direction)
+    {
+        if (direction != Vector2.zero)
+        {
+            int count = _rigidbody2D.Cast(
+                Vector2.zero, 
+                movementContactFilter, 
+                _hitBuffer,
+                moveSpeed * Time.fixedDeltaTime + collisionOffset);
+
+            if (count == 0)
+            {
+                _rigidbody2D.MovePosition(_rigidbody2D.position + direction * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            }
+            else
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    Vector2 currentNormal = _hitBuffer[i].normal;
+                    Vector2 alongNormal = Vector2.Dot(direction, currentNormal) * currentNormal;
+                    Vector2 tangentMovement = direction - alongNormal;
+                    if (_rigidbody2D.Cast(tangentMovement, movementContactFilter, _hitBuffer, moveSpeed * Time.fixedDeltaTime + collisionOffset) == 0)
+                    {
+                        _rigidbody2D.MovePosition(_rigidbody2D.position + tangentMovement * moveSpeed * Time.fixedDeltaTime);
+                        return true;
+                    }
+                }
+                return false;
+            }
+               
+        }
+        else
+            return false;
+    }
+
+    /*private bool CanMoveTo(Vector3 targetPos)
     {
         Bounds bounds = _boxCollider.bounds;
         bounds.center = targetPos;
@@ -70,4 +120,5 @@ public class PlayerMovement : MonoBehaviour
         isMoving = false;
         _animator.SetBool("IsMoving", false);
     }
+    */
 }
